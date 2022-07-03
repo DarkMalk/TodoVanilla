f526dd3e2a713ecad0e887ca671252fa6c781f33
DarkMalk <sebastian.cea40@gmail.com>
2022-07-02 23:10:38 -0400

    Add function delete task

--- main.js
+++ main.js
@@ -7,14 +7,20 @@ const btnSubmit = document.querySelector("#btnSubmit")
 const form = document.querySelector("form")
 
 let data = {}
-
-if (localStorage.getItem("tasks")) {
-  data = JSON.parse(localStorage.getItem("tasks"))
+console.log(data.length)
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("tasks")) {
+    data = JSON.parse(localStorage.getItem("tasks"))
+  }
   printTask()
-} else {
-  contTask.innerHTML = `
-    <div class="card-task" style="justify-content: center;">Not found task</div>
-  `
+})
+
+function deleteTask(e) {
+  if (e.target.classList.contains("btn-delete")) {
+    delete data[e.target.dataset.id]
+    printTask()
+    saveTask()
+  }
 }
 
 function saveTask() {
@@ -22,13 +28,21 @@ function saveTask() {
 }
 
 function printTask() {
+  contTask.innerHTML = ""
   Object.keys(data).forEach((element) => {
-    contTask.innerHTML = ""
     let clone = template.cloneNode(true)
     clone.querySelector("#nameTaskCard").textContent = data[element].name
     clone.querySelector("#descriptionTaskCard").textContent = data[element].desc
+    clone.querySelector("#buttonCheck").dataset.id = data[element].id
+    clone.querySelector("#buttonDelete").dataset.id = data[element].id
     fragment.appendChild(clone)
   })
+  if (Object.keys(data).length === 0) {
+    contTask.innerHTML = `
+      <div class="card-task" style="justify-content: center;">Not found task</div>
+    `
+  }
+
   contTask.appendChild(fragment)
 }
 
@@ -41,11 +55,17 @@ btnSubmit.addEventListener("click", (e) => {
       id: Date.now(),
     }
     data[value.id] = value
+
     printTask()
     saveTask()
+    form.reset()
   } else {
     alert("Complete Name and Description!")
   }
-
+  e.stopPropagation()
   e.preventDefault()
+})
+
+contTask.addEventListener("click", (e) => {
+  deleteTask(e)
 })
