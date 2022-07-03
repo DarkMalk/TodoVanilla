b5e02c72d3ad8e0517ea7f69b40fd8be32364bda
DarkMalk <sebastian.cea40@gmail.com>
2022-07-03 02:48:12 -0400

    Add funcionality task completed and reload task, deleted task

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const btnSubmit = document.querySelector("#btnSubmit")
 const form = document.querySelector("form")
 
 let data = {}
-console.log(data.length)
+
 document.addEventListener("DOMContentLoaded", () => {
   if (localStorage.getItem("tasks")) {
     data = JSON.parse(localStorage.getItem("tasks"))
@@ -23,6 +23,17 @@ function deleteTask(e) {
   }
 }
 
+function completedTask(e) {
+  if (e.target.classList.contains("btn-check")) {
+    data[e.target.dataset.id].state = true
+  }
+  if (e.target.classList.contains("btn-reload")) {
+    data[e.target.dataset.id].state = false
+  }
+  printTask()
+  saveTask()
+}
+
 function saveTask() {
   localStorage.setItem("tasks", JSON.stringify(data))
 }
@@ -35,6 +46,18 @@ function printTask() {
     clone.querySelector("#descriptionTaskCard").textContent = data[element].desc
     clone.querySelector("#buttonCheck").dataset.id = data[element].id
     clone.querySelector("#buttonDelete").dataset.id = data[element].id
+
+    if (data[element].state) {
+      clone
+        .querySelector("#buttonCheck")
+        .classList.replace("fa-check", "fa-rotate-right")
+      clone
+        .querySelector("#buttonCheck")
+        .classList.replace("btn-check", "btn-reload")
+      clone.querySelector("#nameTaskCard").classList.add("completedTask")
+      clone.querySelector("#descriptionTaskCard").classList.add("completedTask")
+    }
+
     fragment.appendChild(clone)
   })
   if (Object.keys(data).length === 0) {
@@ -68,4 +91,5 @@ btnSubmit.addEventListener("click", (e) => {
 
 contTask.addEventListener("click", (e) => {
   deleteTask(e)
+  completedTask(e)
 })
